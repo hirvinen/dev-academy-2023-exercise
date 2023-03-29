e179b8886d2c50ce09c3699ab2c529b1c6623c1a
Patrik Hirvinen <patrik.hirvinen@iki.fi>
2023-03-29 07:02:17 +0300

    #15 Add prettier, eslint-config-prettier, basic config and .prettierignore.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,6 +18,7 @@ module.exports = {
         'plugin:@typescript-eslint/recommended',
         'plugin:@typescript-eslint/recommended-requiring-type-checking',
         'airbnb-typescript',
+        'prettier',
       ],
       parserOptions: {
         project: [
